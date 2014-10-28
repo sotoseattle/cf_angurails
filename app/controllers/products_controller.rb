8402e9412e27f7e1c2e0f7f70ba95fa4128ad4f3
Javier Soto <sotoseattle@gmail.com>
2014-10-28 15:54:36 -0700

    add create product end point

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,20 @@ class ProductsController < ApplicationController
     end
     render json: products, status: 200
   end
+
+  def create
+    product = Product.new(product_params)
+    if product.save
+      render json: product, status: 201, location: product
+    else
+      render json: product.errors, status: 422
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price,
+      :shine, :faces, :rarity, :color)
+  end
 end
---
+++ test/integration/creating_products_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class CreatingProductsTest < ActionDispatch::IntegrationTest
+  test 'create a product with valid data' do
+    post '/products', { product: {
+      name: 'Ruby',
+      description: 'The most beautiful of them all',
+      price: '999.99',
+      shine: "99",
+      faces: 7,
+      rarity: 5,
+      color: 'red'
+    }}.to_json, {
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json'
+    }
+
+    assert_equal 201, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    product = json(response.body)
+    assert_equal product_url(product[:id]), response.location
+
+    assert_equal 'Ruby', product[:name]
+    assert_equal 999.99, product[:price]
+    assert_equal "99", product[:shine]
+    assert_equal 7, product[:faces]
+    assert_equal 5, product[:rarity]
+    assert_equal 'red', product[:color]
+    assert_equal 'The most beautiful of them all', product[:description]
+  end
+
+  test 'does not create a product with invalid data' do
+    post '/products', { product: {
+      name: nil,
+      description: 'The most beautiful of them all',
+      price: '999.99',
+      shine: "99",
+      faces: 7,
+      rarity: 5,
+      color: 'red'
+    }}.to_json, {
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json'
+    }
+
+    assert_equal 422, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    # product = json(response.body)
+    # assert_equal product_url(product[:id]), response.location
+  end
+end
